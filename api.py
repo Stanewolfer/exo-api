930f41c0e761bbb0e40e18725b23e96372f2ce41
Stanewolfer <seiya0500@gmail.com>
2023-10-30 10:08:31 +0100

    fichier année bissextile

---
+++ api.py
@@ -0,0 +1,11 @@
+def isBissextile(annee):
+    if annee % 4 == 0:
+        if annee % 100 == 0:
+            if annee % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
